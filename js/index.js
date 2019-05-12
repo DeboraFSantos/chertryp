54e420373a7771442e1b91e78f76efeac5c22942
Débora Ferreira <debora.ferreira.s@hotmail.com>
2019-05-12 02:02:31 -0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+	$(document).ready(function(){
+
+		var ulNav= "#nav", openNav = "active";	
+
+		$('.toggle-menu').click(function(e){
+
+				if($(ulNav).hasClass(openNav)){
+					$(ulNav).removeClass(openNav);
+				}else{
+					$(ulNav).addClass(openNav);
+				}
+				e.preventDefault();
+		})			
+	})
